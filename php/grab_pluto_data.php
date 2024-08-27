50c3b8d6f4bc9c3d3f06f0288a3df15a575b9e74
Michell Smith <79624312+gogetta69@users.noreply.github.com>
2024-08-27 16:25:17 -0600

    Update grab_pluto_data.php

--- php/grab_pluto_data.php
+++ php/grab_pluto_data.php
@@ -1,6 +1,7 @@
 <?php
 
 function generateUuidV4() {
+    // Generate a random UUID v4
     $data = bin2hex(random_bytes(16));
     return sprintf('%s-%s-%s-%s-%s',
         substr($data, 0, 8),
@@ -12,6 +13,7 @@ function generateUuidV4() {
 }
 
 function generateShortHexSid($length) {
+    // Generate a random hexadecimal string of the specified length
     $chars = 'abcdef0123456789';
     $sid = '';
     for ($i = 0; $i < $length; $i++) {
@@ -23,38 +25,70 @@ function generateShortHexSid($length) {
 function grabEPG() {
     echo '[INFO] Grabbing EPG...' . PHP_EOL;
 
-    $url = "https://i.mjh.nz/PlutoTV/.app.json";
-    $response = file_get_contents($url);
-    $data = json_decode($response, true);
+    // Start and stop times
+    $startTime = urlencode(date('Y-m-d H:00:00.000O'));
+    $stopTime = urlencode(date('Y-m-d H:00:00.000O', strtotime('+48 hours')));
 
-    if (isset($data['regions']['us'])) {
-        $channels = $data['regions']['us']['channels'];
-        processEPG($channels);
-    } else {
-        echo '[ERROR] US region not found in data.' . PHP_EOL;
-    }
+    $url = "http://api.pluto.tv/v2/channels?start={$startTime}&stop={$stopTime}";
+    echo $url . PHP_EOL;
+
+    $response = file_get_contents($url);
+    $channels = json_decode($response, true);
+
+    processEPG($channels);
 }
 
 function processEPG($channels) {
+    // Hardcoded URL for the EPG file
     $epgUrl = "https://raw.githubusercontent.com/gogetta69/public-files/main/Pluto-TV/us.xml";
+
+    // Add the url-tvg attribute to the M3U8 playlist
     $m3u8 = "#EXTM3U url-tvg=\"{$epgUrl}\"\n";
     $tvElements = [];
     $processedPrograms = [];
+	
+	// Generate new deviceId and sid
+    $deviceId = generateUuidV4();
+    $sid = generateShortHexSid(12);
 
-    foreach ($channels as $channelId => $channel) {
-        if (isset($channel['url'])) {
-            $m3uUrl = $channel['url'];
+    foreach ($channels as $channel) {
+        if ($channel['isStitched']) {
+            $m3uBaseUrl = explode('?', $channel['stitched']['urls'][0]['url'])[0];
 
-            $m3u8 .= "#EXTINF:0 tvg-id=\"{$channelId}\" tvg-logo=\"{$channel['logo']}\" group-title=\"{$channel['group']}\", {$channel['name']}\n{$m3uUrl}\n\n";
+            $queryParams = [
+                'advertisingId' => '',
+                'appName' => 'web',
+                'appVersion' => '5.2.2-d60060c7283e0978cc63ba036956b5c1657f8eba',
+                'architecture' => '',
+                'buildVersion' => '',
+                'clientTime' => '',
+                'deviceDNT' => '0',
+                'deviceId' => $deviceId,
+                'deviceLat' => '33.6647',
+                'deviceLon' => '-117.1743',
+                'deviceMake' => 'Chrome',
+                'deviceModel' => 'Chrome',
+                'deviceType' => 'web',
+                'deviceVersion' => '80.0.3987.149',
+                'includeExtendedEvents' => 'false',
+                'marketingRegion' => 'US',
+                'sid' => $sid,
+                'userId' => ''
+            ];
+
+            $queryString = http_build_query($queryParams);
+            $m3uUrl = "{$m3uBaseUrl}?{$queryString}";
+
+            $m3u8 .= "#EXTINF:0 tvg-id=\"{$channel['slug']}\" tvg-logo=\"{$channel['colorLogoPNG']['path']}\" group-title=\"{$channel['category']}\", {$channel['name']}\n{$m3uUrl}\n\n";
 
             echo '[INFO] Adding ' . $channel['name'] . ' channel.' . PHP_EOL;
 
             $channelElement = [
                 'name' => 'channel',
-                'attrs' => ['id' => $channelId],
+                'attrs' => ['id' => $channel['slug']],
                 'children' => [
                     ['name' => 'display-name', 'text' => $channel['name']],
-                    ['name' => 'icon', 'attrs' => ['src' => $channel['logo']]]
+                    ['name' => 'icon', 'attrs' => ['src' => $channel['colorLogoPNG']['path']]]
                 ]
             ];
             $tvElements[] = $channelElement;
@@ -62,26 +96,22 @@ function processEPG($channels) {
             echo "[DEBUG] Skipping 'fake' channel " . $channel['name'] . '.' . PHP_EOL;
         }
 
-        if (isset($channel['programs'])) {
-            foreach ($channel['programs'] as $programme) {
-                $start = date('YmdHis O', $programme[0]);
-                $stop = date('YmdHis O', strtotime('+2 hours', $programme[0])); // Adjust the duration as needed
+        if (isset($channel['timelines'])) {
+            foreach ($channel['timelines'] as $programme) {
+                $uniqueKey = $channel['slug'] . $programme['start'] . $programme['stop'];
 
-                $programmeElement = [
-                    'name' => 'programme',
-                    'attrs' => [
-                        'start' => $start,
-                        'stop' => $stop,
-                        'channel' => $channelId
-                    ],
-                    'children' => [
-                        ['name' => 'title', 'attrs' => ['lang' => 'en'], 'text' => $programme[1]],
-                        ['name' => 'category', 'attrs' => ['lang' => 'en'], 'text' => $channel['group']],
-                        // Add more elements as needed
-                    ]
-                ];
+                if (isset($processedPrograms[$uniqueKey])) {
+                    continue;
+                }
 
-                $tvElements[] = $programmeElement;
+                echo '[INFO] Adding instance of ' . $programme['title'] . ' to channel ' . $channel['name'] . '.' . PHP_EOL;
+
+                $programmeElement = createProgrammeElement($channel, $programme);
+                if ($programmeElement) {
+                    $tvElements[] = $programmeElement;
+                }
+
+                $processedPrograms[$uniqueKey] = true;
             }
         }
     }
@@ -100,7 +130,34 @@ function processEPG($channels) {
     }
 }
 
+function createProgrammeElement($channel, $programme) {
+    try {
+        return [
+            'name' => 'programme',
+            'attrs' => [
+                'start' => formatTime($programme['start']),
+                'stop' => formatTime($programme['stop']),
+                'channel' => $channel['slug']
+            ],
+            'children' => [
+                ['name' => 'title', 'attrs' => ['lang' => 'en'], 'text' => $programme['title']],
+                ['name' => 'desc', 'attrs' => ['lang' => 'en'], 'text' => $programme['episode']['description']],
+                ['name' => 'category', 'attrs' => ['lang' => 'en'], 'text' => $channel['category']],
+                ['name' => 'icon', 'attrs' => ['src' => $programme['episode']['poster']['path']]]
+            ]
+        ];
+    } catch (Exception $e) {
+        echo '[ERROR] Failed to create programme element: ' . $e->getMessage() . PHP_EOL;
+        return null;
+    }
+}
+
 function createElementFromJson($parent, $json) {
+    if (empty($json['name'])) {
+        echo 'Error: Missing name in JSON object.' . PHP_EOL;
+        return;
+    }
+
     $element = $parent->addChild($json['name']);
 
     if (isset($json['attrs'])) {
@@ -120,8 +177,12 @@ function createElementFromJson($parent, $json) {
     }
 }
 
+function formatTime($timestamp) {
+    return date('YmdHis O', strtotime($timestamp));
+}
+
 function saveFile($filename, $content) {
-    $filePath = $_SERVER['DOCUMENT_ROOT'] . '/' . $filename;
+    $filePath = $_SERVER['DOCUMENT_ROOT'] . $filename;
     file_put_contents($filePath, $content);
     echo "[SUCCESS] Saved file: {$filename}" . PHP_EOL;
 }
